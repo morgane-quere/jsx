f6c66610e22e79133a0d88506f298949dbb457de
morgane quere <qu.morgane@gmail.com>
2021-03-30 10:20:35 +0200

    add a react component

--- src/index.js
+++ src/index.js
@@ -2,9 +2,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 // Create a react component
 const App = () => {
-  return <div>Hi there!</div>;
+  const buttonText = 'Click me!';
+
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style={ { backgroundColor: 'blue', color: 'white' } }>{ buttonText }</button>
+    </div>
+  );
 };
 
 // Take the react component and show it on screen
